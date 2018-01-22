f4d19491d02d606b57c8704a741d55d6839f6255
Maksim Petrov <maksimallist@gmail.com>
2018-01-22 18:19:39 +0300

    refactor: map_fn for attention cycle;
    delete second cycle using tf.map_fn().

--- neftagger.py
+++ neftagger.py
@@ -34,162 +34,59 @@ def input_block(x, seq_lens, drop, lstm_units):
     return outputs, final_state, state_size
 
 
-# def attention_block(hidden_states, state_size, window_size, dim_hlayer, batch_size,
-#                     activation, L, sketches_num, discount_factor):
-#
-#     with tf.name_scope("sketching"):
-#
-#         def conv_r(padded_matrix, r):
-#             """
-#             Extract r context columns around each column and concatenate
-#             :param padded_matrix: batch_size x L+(2*r) x 2*state_size
-#             :param r: context size
-#             :return:
-#             """
-#             # gather indices of padded
-#             time_major_matrix = tf.transpose(padded_matrix,
-#                                              [1, 2, 0])  # time-major  -> L x 2*state_size x batch_size
-#             contexts = []
-#             for j in np.arange(r, L + r):
-#                 # extract 2r+1 rows around i for each batch
-#                 context_j = time_major_matrix[j - r:j + r + 1, :, :]  # 2*r+1 x 2*state_size x batch_size
-#                 # concatenate
-#                 context_j = tf.reshape(context_j,
-#                                        [(2 * r + 1) * 2 * state_size, batch_size])  # (2*r+1)*(state_size) x batch_size
-#                 contexts.append(context_j)
-#             contexts = tf.stack(contexts)  # L x (2*r+1)* 2*(state_size) x batch_size
-#             batch_major_contexts = tf.transpose(contexts, [2, 0, 1])
-#             # switch back: batch_size x L x (2*r+1)*2(state_size) (batch-major)
-#             return batch_major_contexts
-#
-#         def constrained_softmax(input_tensor, b, temp=1.0):
-#             """
-#             Compute the constrained softmax (csoftmax);
-#             See paper "Learning What's Easy: Fully Differentiable Neural Easy-First Taggers"
-#             on https://andre-martins.github.io/docs/emnlp2017_final.pdf (page 4)
-#
-#             :param input_tensor: input tensor
-#             :param b: cumulative attention see paper
-#             :param temp: softmax temperature
-#             :return: distribution
-#             """
-#
-#             # input_tensor = tf.reduce_mean(input_tensor)
-#             z = tf.reduce_sum(tf.exp(input_tensor/temp), axis=1, keep_dims=True)
-#             a = tf.exp(input_tensor/temp) * (b/temp) / z
-#             # a = tf.exp(input_tensor/temp) * b / z
-#             u = tf.ones_like(b) - b
-#             t_mask = tf.to_float(tf.less_equal(a, u))
-#             f_mask = tf.to_float(tf.less(u, a))
-#             A = a * t_mask
-#             U = u * f_mask
-#
-#             csoftmax = A + U
-#
-#             return csoftmax
-#
-#         def sketch_step(tensor, cum_attention, hidden_dim):
-#
-#             with tf.variable_scope("W_hh_scope", reuse=tf.AUTO_REUSE):
-#                 W_hh = tf.get_variable(name="W_hh", shape=[2 * state_size * (2 * window_size + 1), state_size],
-#                                        initializer=tf.contrib.layers.xavier_initializer(uniform=True,
-#                                                                                         dtype=tf.float32))
-#             with tf.variable_scope("w_h_scope", reuse=tf.AUTO_REUSE):
-#                 w_h = tf.get_variable(name="w_z", shape=[state_size],
-#                                       initializer=tf.random_uniform_initializer(dtype=tf.float32))
-#
-#             with tf.variable_scope("v_scope", reuse=tf.AUTO_REUSE):
-#                 v = tf.get_variable(name="v", shape=[1, 1, hidden_dim],
-#                                     initializer=tf.random_uniform_initializer(dtype=tf.float32))
-#
-#             with tf.variable_scope("W_hsz_scope", reuse=tf.AUTO_REUSE):
-#                 W_hsz = tf.get_variable(name="W_hsz", shape=[1, 1, 2 * state_size * (2 * window_size + 1), hidden_dim],
-#                                         initializer=tf.contrib.layers.xavier_initializer(uniform=True,
-#                                                                                          dtype=tf.float32))
-#
-#             with tf.variable_scope("w_z_scope", reuse=tf.AUTO_REUSE):
-#                 w_z = tf.get_variable(name="w_z", shape=[1, 1, hidden_dim],
-#                                       initializer=tf.random_uniform_initializer(dtype=tf.float32))
-#
-#             preattention = activation(tf.matmul(tensor, W_hsz) + w_z)  # [batch_size, L, hid_dim]
-#             attention = tf.matmul(preattention, v)  # [batch_size, L]
-#
-#             attentions = attention - cum_attention*discount_factor  # [batch_size, L]
-#             constrained_weights = constrained_softmax(attentions, cum_attention)  # [batch_size, 1]
-#
-#             cn = tf.matmul(tf.expand_dims(constrained_weights, [1]), tensor)  # [batch_size, 1,
-#             #  2*state_size*(2*window_size + 1)]
-#             cn = tf.reshape(cn, [batch_size, 2*state_size*(2*window_size + 1)])  # [batch_size,
-#             #  2*state_size*(2*window_size + 1)]
-#             S = activation(tf.matmul(cn, W_hh) + w_h)  # [batch_size, state_size]
-#
-#             S = tf.matmul(tf.expand_dims(constrained_weights, [2]), tf.expand_dims(S, [1]))  # [batch_size, L,
-#             #  state_size]
-#
-#             return S, constrained_weights
-#
-#         def prepare_tensor(hidstates, sk, padding_col):
-#             hs = tf.concat([hidstates, sk], 2)
-#             # add column on right and left, and add context window
-#             hs = tf.pad(hs, padding_col, "CONSTANT", name="HS_padded")
-#             hs = conv_r(hs, window_size)  # [batch_size, L, 2*state*(2*window_size + 1)]
-#             return hs
-#
-#         sketch = tf.zeros(shape=[batch_size, L, state_size], dtype=tf.float32)  # sketch tenzor
-#         cum_att = tf.zeros(shape=[batch_size, L])  # cumulative attention
-#         padding_hs_col = tf.constant([[0, 0], [window_size, window_size], [0, 0]], name="padding_hs_col")
-#         sketches = []
-#         cum_attentions = []
-#
-#         for i in xrange(sketches_num):
-#             with tf.variable_scope("sketch_loop", reuse=tf.AUTO_REUSE):
-#                 sketch_, cum_att_ = sketch_step(prepare_tensor(hidden_states, sketch, padding_hs_col), cum_att,
-#                                                 dim_hlayer)
-#             sketch += sketch_
-#             cum_att += cum_att_
-#             sketches.append(sketch_)  # list of tensors with shape [batch_size, L, state_size]
-#             cum_attentions.append(cum_att_)  # list of tensors with shape [batch_size, L]
-#
-#     return sketches, cum_attentions
-
-
 # Attention block (B_Block)
 def attention_block(hidden_states, state_size, window_size, dim_hlayer, batch_size,
                     activation, L, sketches_num, discount_factor, temperature):
 
-    with tf.name_scope("sketching"):
+    W_hsz = tf.get_variable(name="W_hsz", shape=[2 * state_size * (2 * window_size + 1), dim_hlayer],
+                            initializer=tf.contrib.layers.xavier_initializer(uniform=True,
+                                                                             dtype=tf.float32))
 
-        def conv_r(padded_matrix, r):
-            """
-            Extract r context columns around each column and concatenate
-            :param padded_matrix: batch_size x L+(2*r) x 2*state_size
-            :param r: context size
-            :return:
-            """
-            # gather indices of padded
-            time_major_matrix = tf.transpose(padded_matrix,
-                                             [1, 2, 0])  # time-major  -> L x 2*state_size x batch_size
-            contexts = []
-            for j in np.arange(r, L + r):
-                # extract 2r+1 rows around i for each batch
-                context_j = time_major_matrix[j - r:j + r + 1, :, :]  # 2*r+1 x 2*state_size x batch_size
-                # concatenate
-                context_j = tf.reshape(context_j,
-                                       [(2 * r + 1) * 2 * state_size, batch_size])  # (2*r+1)*(state_size) x batch_size
-                contexts.append(context_j)
-            contexts = tf.stack(contexts)  # L x (2*r+1)* 2*(state_size) x batch_size
-            batch_major_contexts = tf.transpose(contexts, [2, 0, 1])
-            # switch back: batch_size x L x (2*r+1)*2(state_size) (batch-major)
-            return batch_major_contexts
+    w_z = tf.get_variable(name="w_z", shape=[dim_hlayer],
+                          initializer=tf.random_uniform_initializer(dtype=tf.float32))
 
-        def prepare_tensor(hidstates, sk, padding_col):
-            hs = tf.concat([hidstates, sk], 2)
-            # add column on right and left, and add context window
-            hs = tf.pad(hs, padding_col, "CONSTANT", name="HS_padded")
-            hs = conv_r(hs, window_size)  # [batch_size, L, 2*state*(2*window_size + 1)]
-            return hs
+    v = tf.get_variable(name="v", shape=[dim_hlayer, 1],
+                        initializer=tf.random_uniform_initializer(dtype=tf.float32))
 
-        # TODO: temperature in tf constant
+    W_hh = tf.get_variable(name="W_hh", shape=[2 * state_size * (2 * window_size + 1), state_size],
+                           initializer=tf.contrib.layers.xavier_initializer(uniform=True, dtype=tf.float32))
+
+    w_h = tf.get_variable(name="w_h", shape=[state_size],
+                          initializer=tf.random_uniform_initializer(dtype=tf.float32))
+
+    def conv_r(padded_matrix, r):
+        """
+        Extract r context columns around each column and concatenate
+        :param padded_matrix: batch_size x L+(2*r) x 2*state_size
+        :param r: context size
+        :return:
+        """
+        # gather indices of padded
+        time_major_matrix = tf.transpose(padded_matrix,
+                                         [1, 2, 0])  # time-major  -> L x 2*state_size x batch_size
+        contexts = []
+        for j in np.arange(r, L + r):
+            # extract 2r+1 rows around i for each batch
+            context_j = time_major_matrix[j - r:j + r + 1, :, :]  # 2*r+1 x 2*state_size x batch_size
+            # concatenate
+            context_j = tf.reshape(context_j,
+                                   [(2 * r + 1) * 2 * state_size,
+                                    batch_size])  # (2*r+1)*(state_size) x batch_size
+            contexts.append(context_j)
+        contexts = tf.stack(contexts)  # L x (2*r+1)* 2*(state_size) x batch_size
+        batch_major_contexts = tf.transpose(contexts, [2, 0, 1])
+        # switch back: batch_size x L x (2*r+1)*2(state_size) (batch-major)
+        return batch_major_contexts
+
+    def prepare_tensor(hidstates, sk, padding_col):
+        hs = tf.concat([hidstates, sk], 2)
+        # add column on right and left, and add context window
+        hs = tf.pad(hs, padding_col, "CONSTANT", name="HS_padded")
+        hs = conv_r(hs, window_size)  # [batch_size, L, 2*state*(2*window_size + 1)]
+        return hs
+
+    def sketch_step(tensor, cum_attention, temper):
+
         def constrained_softmax(input_tensor, b, temp):
             """
             Compute the constrained softmax (csoftmax);
@@ -203,8 +100,8 @@ def attention_block(hidden_states, state_size, window_size, dim_hlayer, batch_si
             """
 
             # input_tensor = tf.reduce_mean(input_tensor)
-            z = tf.reduce_sum(tf.exp(input_tensor/temp), axis=1, keep_dims=True)
-            a = tf.exp(input_tensor/temp) * (b/temp) / z
+            z = tf.reduce_sum(tf.exp(input_tensor / temp), axis=1, keep_dims=True)
+            a = tf.exp(input_tensor / temp) * (b / temp) / z
             # a = tf.exp(input_tensor/temp) * b / z
             u = tf.ones_like(b) - b
             t_mask = tf.to_float(tf.less_equal(a, u))
@@ -216,69 +113,43 @@ def attention_block(hidden_states, state_size, window_size, dim_hlayer, batch_si
 
             return csoftmax
 
-        with tf.variable_scope("sketch_loop", reuse=tf.AUTO_REUSE):
-            def sketch_step(tensor, cum_attention, hidden_dim, temper):
+        def attention(t):
+            before_att = activation(tf.matmul(t, W_hsz) + w_z)
+            att = tf.matmul(before_att, v)  # [batch_size, 1]
+            return att
 
-                bs_split = tf.split(tensor, L, axis=1)
-                attentions = []
+        tensor = tf.transpose(tensor, [1, 0, 2])
 
-                with tf.variable_scope("W_hh_scope", reuse=tf.AUTO_REUSE):
-                    W_hh = tf.get_variable(name="W_hh", shape=[2 * state_size * (2 * window_size + 1), state_size],
-                                           initializer=tf.contrib.layers.xavier_initializer(uniform=True, dtype=tf.float32))
+        attentions = tf.map_fn(attention, tensor, dtype=tf.float32)  # [batch_size, 1, L]
+        attentions = tf.reshape(attentions, [batch_size, L]) - cum_attention*discount_factor  # [batch_size, L]
+        constrained_weights = constrained_softmax(attentions, cum_attention, temper)  # [batch_size, L]
 
-                with tf.variable_scope("w_h_scope", reuse=tf.AUTO_REUSE):
-                    w_h = tf.get_variable(name="w_z", shape=[state_size],
-                                          initializer=tf.random_uniform_initializer(dtype=tf.float32))
+        tensor = tf.transpose(tensor, [1, 0, 2])
+        cn = tf.reduce_sum(tensor*tf.expand_dims(constrained_weights, [2]), axis=1)  # [batch_size,
+        #  2*state_size*(2*window_size + 1)]
+        cn = tf.reshape(cn, [batch_size, 2*state_size*(2*window_size + 1)])  # [batch_size,
+        #  2*state_size*(2*window_size + 1)]
+        s = activation(tf.matmul(cn, W_hh) + w_h)  # [batch_size, state_size]
 
-                with tf.variable_scope("v_scope", reuse=tf.AUTO_REUSE):
-                    v = tf.get_variable(name="v", shape=[hidden_dim, 1],
-                                        initializer=tf.random_uniform_initializer(dtype=tf.float32))
+        s = tf.matmul(tf.expand_dims(constrained_weights, [2]), tf.expand_dims(s, [1]))  # [batch_size, L,
+        #  state_size]
 
-                with tf.variable_scope("W_hsz_scope", reuse=tf.AUTO_REUSE):
-                    W_hsz = tf.get_variable(name="W_hsz", shape=[2 * state_size * (2 * window_size + 1), hidden_dim],
-                                            initializer=tf.contrib.layers.xavier_initializer(uniform=True,
-                                                                                             dtype=tf.float32))
+        return s, constrained_weights
 
-                with tf.variable_scope("w_z_scope", reuse=tf.AUTO_REUSE):
-                    w_z = tf.get_variable(name="w_z", shape=[hidden_dim],
-                                          initializer=tf.random_uniform_initializer(dtype=tf.float32))
+    sketch = tf.zeros(shape=[batch_size, L, state_size], dtype=tf.float32)  # sketch tenzor
+    cum_att = tf.zeros(shape=[batch_size, L])  # cumulative attention
+    padding_hs_col = tf.constant([[0, 0], [window_size, window_size], [0, 0]], name="padding_hs_col")
+    temperature = tf.constant(temperature, dtype=tf.float32, name='attention_temperature')
+    sketches = []
+    cum_attentions = []
 
-                for j in xrange(L):
-                    with tf.variable_scope("sketch_step_loop", reuse=tf.AUTO_REUSE):
-                        tensor_i = tf.squeeze(bs_split[i])
-                        preattention = activation(tf.matmul(tensor_i, W_hsz) + w_z)
-                        attention = tf.matmul(preattention, v)  # [batch_size, 1]
-                        attentions.append(attention)
-
-                attentions = tf.stack(attentions, axis=1)  # [batch_size, 1, L]
-                attentions = tf.squeeze(attentions) - cum_attention*discount_factor  # [batch_size, L]
-                constrained_weights = constrained_softmax(attentions, cum_attention, temper)  # [batch_size, L]
-
-                cn = tf.reduce_sum(tensor*tf.expand_dims(constrained_weights, [2]), axis=1)  # [batch_size,
-                #  2*state_size*(2*window_size + 1)]
-                cn = tf.reshape(cn, [batch_size, 2*state_size*(2*window_size + 1)])  # [batch_size,
-                #  2*state_size*(2*window_size + 1)]
-                s = activation(tf.matmul(cn, W_hh) + w_h)  # [batch_size, state_size]
-
-                s = tf.matmul(tf.expand_dims(constrained_weights, [2]), tf.expand_dims(s, [1]))  # [batch_size, L,
-                #  state_size]
-
-                return s, constrained_weights
-
-        sketch = tf.zeros(shape=[batch_size, L, state_size], dtype=tf.float32)  # sketch tenzor
-        cum_att = tf.zeros(shape=[batch_size, L])  # cumulative attention
-        padding_hs_col = tf.constant([[0, 0], [window_size, window_size], [0, 0]], name="padding_hs_col")
-        temperature = tf.constant(temperature, dtype=tf.float32, name='attention_temperature')
-        sketches = []
-        cum_attentions = []
-
-        for i in xrange(sketches_num):
-            sketch_, cum_att_ = sketch_step(prepare_tensor(hidden_states, sketch, padding_hs_col), cum_att,
-                                            dim_hlayer, temperature)
-            sketch += sketch_
-            cum_att += cum_att_
-            sketches.append(sketch_)  # list of tensors with shape [batch_size, L, state_size]
-            cum_attentions.append(cum_att_)  # list of tensors with shape [batch_size, L]
+    for i in xrange(sketches_num):
+        sketch_, cum_att_ = sketch_step(prepare_tensor(hidden_states, sketch, padding_hs_col), cum_att,
+                                        temperature)
+        sketch += sketch_
+        cum_att += cum_att_
+        sketches.append(sketch_)  # list of tensors with shape [batch_size, L, state_size]
+        cum_attentions.append(cum_att_)  # list of tensors with shape [batch_size, L]
 
     return sketches, cum_attentions
 
@@ -422,18 +293,19 @@ class NEF():
             # masked, batch_size x 1 (regularization like dropout but mask)
             # losses = tf.reduce_mean(tf.cast(mask, tf.float32) * tf.transpose(losses, [1, 0]), 1)
             self.losses_reg = tf.reduce_mean(tf.transpose(self.losses, [1, 0]), 1)
-            # regularization
-            W_hh = tf.get_variable(name='W_hh', shape=[2 * state_size * (2 * self.window_size + 1), state_size])
-            if self.l2_scale > 0:
-                weights_list = [W_hh, W_out]  # word embeddings not included
-                l2_loss = tf.contrib.layers.apply_regularization(
-                    tf.contrib.layers.l2_regularizer(self.l2_scale), weights_list=weights_list)
-                self.losses_reg += l2_loss
-            if self.l1_scale > 0:
-                weights_list = [W_hh, W_out]
-                l1_loss = tf.contrib.layers.apply_regularization(
-                    tf.contrib.layers.l1_regularizer(self.l1_scale), weights_list=weights_list)
-                self.losses_reg += l1_loss
+
+        # regularization
+        W_hh = tf.get_variable(name='W_hh', shape=[2 * state_size * (2 * self.window_size + 1), state_size])
+        if self.l2_scale > 0:
+            weights_list = [W_hh, W_out]  # word embeddings not included
+            l2_loss = tf.contrib.layers.apply_regularization(
+                tf.contrib.layers.l2_regularizer(self.l2_scale), weights_list=weights_list)
+            self.losses_reg += l2_loss
+        if self.l1_scale > 0:
+            weights_list = [W_hh, W_out]
+            l1_loss = tf.contrib.layers.apply_regularization(
+                tf.contrib.layers.l1_regularizer(self.l1_scale), weights_list=weights_list)
+            self.losses_reg += l1_loss
 
         # gradients and update operation for training the model
         if self.mode == 'train':
